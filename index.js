431029fc521863792d206816fdc5022216d3885d
marcelo <mhosan@gmail.com>
2020-11-06 11:06:44 -0300

    na

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 //middleware
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
