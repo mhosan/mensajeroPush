09c9c111f2e58209ba47b2759a45bbdff762bfd1
marcelo <mhosan@gmail.com>
2020-11-11 12:22:12 -0300

    guardando

--- modelos/msg.js
+++ modelos/msg.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var msgSchema = new Schema({
-    fechaAlta : Date,
-    msg : {
+    fechaAlta: Date,
+    msg: {
         title: String,
         message: String
     },
-    keys : {
-        p256dh : String,
-        auth : String
+    keys: {
+        p256dh: String,
+        auth: String
     }
-});
+},
+    { versionKey: false }
+);
 
 module.exports = mongoose.model('Mensajes', msgSchema);
--- modelos/subsc.js
+++ modelos/subsc.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var subscripcionesSchema = new Schema({
-    fechaAlta : Date,
-    endpoint : String,
-    expirationTime : Date,
-    keys : {
-        p256dh : String,
-        auth : String
+    fechaAlta: Date,
+    endpoint: String,
+    expirationTime: Date,
+    keys: {
+        p256dh: String,
+        auth: String
     }
-});
+},
+    { versionKey: false }
+);
 
 module.exports = mongoose.model('Suscripciones', subscripcionesSchema);
